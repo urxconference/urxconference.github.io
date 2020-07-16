b50166b8bc564d2e397108f3c4e03e483c007683
Wade Fletcher <wbfletch@gmail.com>
2020-07-16 01:43:25 -0400

    Make design fully responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navy: {
+          100: "#d5d7e1",
+          200: "#abaec3",
+          300: "#8086a6",
+          400: "#565d88",
+          500: "#2c356a",
+          600: "#232a55",
+          700: "#1a2040",
+          800: "#12152a",
+          900: "#090b15"
+},
+      }
+    },
   },
   variants: {},
   plugins: [],
